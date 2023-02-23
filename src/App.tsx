0013a241a878bb18175410482556b19f6301d58d
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-02-23 15:14:02 +0200

    metods Array

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 function App() {
-  const clickMe = () => {
-    console.log("Click");
-  };
-  return (
-    <div className="App">
-      <button onClick={clickMe}>Click</button>
-    </div>
-  );
+  //metods Array
+  const people = [
+    { name: "Nikita", age: 29, proffesion: "FronEnd" },
+    { name: "Alex", age: 30, proffesion: "PM" },
+    { name: "Look", age: 35, proffesion: "Sells" },
+    { name: "Just", age: 32, proffesion: "Sellers" },
+    { name: "Only", age: 21, proffesion: "Auto" },
+    { name: "Early", age: 28, proffesion: "Credit" },
+  ];
+
+  // ForEach
+
+  //Map
+
+  //Filter
+
+  //Find
+
+  //FindIndex
+
+  return <div className="App"></div>;
 }
 
 export default App;
