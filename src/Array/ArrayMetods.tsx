ec670f5143bf85cf90a22bfa1cf56ef3cd36f517
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-01 13:13:51 +0200

    add components Array

---
+++ src/Array/ArrayMetods.tsx
@@ -0,0 +1,96 @@
+const ArrayMetods = () => {
+  const people = [
+    { name: "Nikita", age: 29, proffesion: "FronEnd" },
+    { name: "Alex", age: 30, proffesion: "PM" },
+    { name: "Look", age: 35, proffesion: "Sells" },
+    { name: "Just", age: 32, proffesion: "Sellers" },
+    { name: "Only", age: 17, proffesion: "Auto" },
+    { name: "Early", age: 15, proffesion: "Credit" },
+  ];
+  // -------------------------------------------------------
+  // Default works with Array ES5
+  // for (let i = 0; i < people.length; i++) {
+  //   console.log(people[i]);
+  // }
+  //
+  //
+  // -------------------------------------------------------
+  // Default works with Array ES6
+  // for (let person of people) {
+  //   console.log(person);
+  // }
+  //
+  //
+  // -------------------------------------------------------
+  // ForEach
+  // people.forEach(function (person, index, pArr) {
+  //   console.log(person);
+  //   console.log(index);
+  //   console.log(pArr);
+  // });
+  //---
+  //
+  // people.forEach((person) => console.log(person));
+  //
+  //
+  // -------------------------------------------------------
+  // Map
+  // const newPeople = people.map((person) => {
+  // return person;
+  // return "Hello";
+  // return person.age;
+  // return `${person.name} (${person.age})`;
+  // });
+  // console.log(newPeople); // metod Map return new Array
+  //
+  //
+  // -------------------------------------------------------
+  // Filter
+  // const adults = []; //ES5
+  // for (let i = 0; i < people.length; i++) {
+  //   if (people[i].age >= 18) {
+  //     adults.push(people[i]);
+  //   }
+  // }
+  // console.log(adults);
+  //---
+  //
+  // const adults = people.filter((person) => {
+  //   if (person.age >= 18) {
+  //     return true;
+  //   }
+  // });
+  // console.log(adults)
+  //---
+  //
+  // const adults = people.filter((person) => person.age >= 18);
+  // console.log(adults);
+  //
+  //
+  // -------------------------------------------------------
+  // Reduce
+  // let ages = 0;
+  // for (let i = 0; i < people.length; i++) {
+  //   ages += people[i].age;
+  // }
+  // console.log(ages);
+  //
+  //
+  // const ages = people.reduce((total, person) => {
+  //   return total + person.age;
+  // }, 0);
+  // console.log(ages);
+  //
+  //
+  // -------------------------------------------------------
+  // Find
+  //
+  //
+  // -------------------------------------------------------
+  // FindIndex
+  //
+  //
+  // -------------------------------------------------------
+  return <div>ArrayMetods</div>;
+};
+export default ArrayMetods;
