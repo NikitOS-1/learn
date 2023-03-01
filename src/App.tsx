acb9a69fcec985e4f5cd194636c0806d47c8f9d9
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-01 17:55:04 +0200

    add redux typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App">Hello</div>;
+  const [count, setCount] = useState<number>(0);
+  const increment = () => {
+    setCount((prevState) => prevState + 1);
+  };
+  const decrement = () => {
+    setCount((prevState) => prevState - 1);
+  };
+  return (
+    <div className="App">
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <div>{count}</div>
+    </div>
+  );
 }
 
 export default App;
