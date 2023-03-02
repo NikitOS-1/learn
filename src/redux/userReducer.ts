572913f97337487906dc4caabdc07eddcb690a47
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-02 11:19:54 +0200

    add payload

--- src/redux/userReducer.ts
+++ src/redux/userReducer.ts
@@ -2,21 +2,30 @@ import { createSlice } from "@reduxjs/toolkit";
 
 interface UserState {
   user: boolean;
+  id: number;
+  name: string;
 }
 
 const initialState: UserState = {
   user: false,
+  id: 0,
+  name: "",
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    isUser: (state) => {
-      state.user = true;
+    isUser: (state, action) => {
+      if (action.payload == 1) {
+        state.user = true;
+      }
+      action.payload.name = "sss";
     },
-    notUser: (state) => {
-      state.user = false;
+    notUser: (state, action) => {
+      if (action.payload == 1) {
+        state.user = false;
+      }
     },
   },
 });
