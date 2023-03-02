d2dace9e40b19160c72e40412c3b890898a96aac
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-02 11:19:45 +0200

    add payload

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,19 @@ function App() {
   const counts = useAppSelector((state) => state.counter.value);
   const user = useAppSelector((state) => state.isUser.user);
   const dispatch = useAppDispatch();
+  let id = 1;
+  let name = "nik";
+  console.log(id);
+  console.log(name);
   console.log(user);
   return (
     <div className="App">
       <button onClick={() => dispatch(increment())}>increment</button>
       <button onClick={() => dispatch(decrement())}>decrement</button>
       <div>{counts}</div>
-      <button onClick={() => dispatch(isUser())}>if you user click</button>
-      <button onClick={() => dispatch(notUser())}>if you user click</button>
+      <button onClick={() => dispatch(isUser({ id, name }))}>
+        if you are user
+      </button>
     </div>
   );
 }
