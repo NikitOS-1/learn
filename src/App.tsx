a9593a87e6944558f5b2f0ad64e65191540324aa
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-15 22:46:35 +0200

    add Navigate in Welcome to Login

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
         <Route path="/Registration" element={<Registration />} />
         <Route path="/Welcome" element={<Welcome />} />
       </Routes>
+      <Welcome />
     </div>
   );
 }
--- src/pages/Login/Welcome.tsx
+++ src/pages/Login/Welcome.tsx
@@ -1,6 +1,9 @@
+import { Navigate } from "react-router-dom";
+
 const Welcome = () => {
   return (
     <div>
+      <Navigate to={"/"} />
       <div>Welcome</div>
       <button>Log Out</button>
     </div>
