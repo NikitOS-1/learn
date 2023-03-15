0400d4094701514f9b8d91c220eadf0910884b4f
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-15 22:17:15 +0200

    add jsconfig compilerOptions

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <SignUp />
-      <Registration />
-      <Welcome />
+      {/* <Registration />
+      <Welcome /> */}
     </div>
   );
 }
