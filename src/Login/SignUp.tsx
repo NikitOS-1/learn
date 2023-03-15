e3a0503760973a5868c6d1b866393c7275c8c15a
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-15 21:44:45 +0200

    add new componets

---
+++ src/Login/SignUp.tsx
@@ -0,0 +1,23 @@
+const SignUp = () => {
+  return (
+    <div>
+      <form action="">
+        <div>
+          <h5>Login</h5>
+          <input type="text" placeholder="login" />
+        </div>
+        <div>
+          <h5>Password</h5>
+          <input type="text" placeholder="password" />
+        </div>
+        <br />
+        <br />
+        <button>Login</button>
+        <br />
+        <br />
+        <a href="">Create account</a>
+      </form>
+    </div>
+  );
+};
+export default SignUp;
