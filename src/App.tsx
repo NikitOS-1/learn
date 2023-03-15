f651ac80518e158c9c1f74a2c1ee1afea71ecf54
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-15 21:49:34 +0200

    ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Registration from "./Login/Registration";
+import SignUp from "./Login/SignUp";
+import Welcome from "./Login/Welcome";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SignUp />
+      <Registration />
+      <Welcome />
+    </div>
+  );
 }
 
 export default App;
--- src/Login/Registration.tsx
+++ src/Login/Registration.tsx
@@ -1,4 +1,31 @@
 const Registration = () => {
-  return <div>Registration</div>;
+  return (
+    <div>
+      <form action="">
+        <div>
+          <h5>First Name</h5>
+          <input type="text" placeholder="First Name" />
+        </div>
+        <div>
+          <h5>Last Name</h5>
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <div>
+          <h5>Login</h5>
+          <input type="text" placeholder="login" />
+        </div>
+        <div>
+          <h5>Password</h5>
+          <input type="text" placeholder="password" />
+        </div>
+        <div>
+          <h5>Password</h5>
+          <input type="text" placeholder="password" />
+        </div>
+        <br />
+        <button>Registration</button>
+      </form>
+    </div>
+  );
 };
 export default Registration;
