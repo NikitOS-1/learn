7bbcd34ec58c7e21d77fa656212cdde4aeea7eda
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-18 21:41:54 +0200

    learn useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import { Route, Routes } from "react-router-dom";
 import RegistrationPage from "./pages/Authentication/RegistrationPage";
 import LoginPage from "./pages/Authentication/LoginPage";
 import HomePage from "./pages/Authentication/HomePage";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+
+  const addNumber = () => {
+    setCount((state) => state + 1);
+  };
+  useEffect(() => {
+    if (count == 2) {
+      console.log(count);
+    }
+  });
   return (
     <div className="App">
+      <div>count:{count}</div>
+      <button onClick={addNumber}>Click</button>
       {/* <Routes> */}
       {/* <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
