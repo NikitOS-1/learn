10beabcb31b63b322497f0ee6e7a87b7d81d85c5
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-03-20 15:47:28 +0200

    add UseEf

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import RegistrationPage from "./pages/Authentication/RegistrationPage";
 import LoginPage from "./pages/Authentication/LoginPage";
 import HomePage from "./pages/Authentication/HomePage";
 import { useEffect, useState } from "react";
+import UseEf from "useEf/UseEf";
 
 function App() {
   return (
     <div className="App">
+      <UseEf />
       {/* <Routes> */}
       {/* <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
---
+++ src/useEf/UseEf.jsx
@@ -0,0 +1,4 @@
+const UseEf = () => {
+  return <div>UseEf</div>;
+};
+export default UseEf;
